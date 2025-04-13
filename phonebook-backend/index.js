fba295769061e3a42bde7e103455eef11bef6512
Keybats <smule2006@outlook.com>
2025-04-13 13:05:24 +0300

    uibuild

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -7,6 +7,7 @@ const { runInNewContext } = require('vm')
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
+const { pseudoRandomBytes } = require('crypto')
 
 let persons = [
   { 
@@ -34,7 +35,7 @@ let persons = [
 
 
 
-app.use(cors())
+//app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
@@ -50,26 +51,53 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  response.send(
-    `<div> Phonebook has info for ${persons.length} people <br/> <br/> ${Date()} </div>`
-  )
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findById(id).then(person => {
-    response.json(person)
+  Person.find({}).then(result => {
+    const peopleAmount = result.length
+    response.send(
+      `<div> Phonebook has info for ${peopleAmount} people <br/> <br/> ${Date()} </div>`
+    )
   })
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  console.log("deleted", id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+  .catch(error => next(error))
+})
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      person.name = name
+      person.number = number
+  
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+
+})
 
 app.post('/api/persons', (reqest, response) => {
   const body = reqest.body
@@ -79,18 +107,56 @@ app.post('/api/persons', (reqest, response) => {
     })
   }
   
-  const person = new Person ({
-    name: body.name,
-    number: body.number
-  })
+  
+    
+    Person.find({name: body.name})
+      .then(person => {
+        
+        if (person[0]) {
+          person[0].number = body.number
 
-  person.save().then(savedPerson => {
-    response.json((savedPerson))
-  })
+          return person[0].save().then(updatedPerson => {
+            response.json(updatedPerson)
+          })
+        }
+        else {
+          const person = new Person ({
+            name: body.name,
+            number: body.number
+          })
+        
+          person.save().then(savedPerson => {
+            response.json((savedPerson))
+          })
+        }
+      })
+  
+
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+// Ask about having to submit multiple versions of the note app in discord
